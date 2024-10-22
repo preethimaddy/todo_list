c106cb4a25eb55b97774f48b3c597ff9065f6079
Preethi Madhavan <98338783+preethimaddy@users.noreply.github.com>
2024-10-22 17:04:47 +0530

    Todo task adding

--- src/ToDo.jsx
+++ src/ToDo.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const ToDo = () => {
+
+const ToDo = ()=> {
+
+  const[todos, setTodos] = useState([]); // State for the list of to-dos
+  const[input, setInput] = useState(" "); // State for the input text
+
+
+   // Function to handle adding a new to-do
+const addToDo = () => {
+  if(input.trim () !== ""){
+    setTodos([...todos, input]); // Add new to-do to the list
+      setInput(""); // Clear input field
+  }
+}
   return (
     <>
-    <div  className = "todo">ToDo List</div>
+<div>
+<h1>To-Do List</h1>
+<input type="text" 
+        value={input}
+        onChange={(e)=> setInput(e.target.value)}
+        placeholder="Add a new task" />
+<button onClick={addToDo}>Add</button>
+<ol>
+  {todos.map((todo,index)=>
+(
+  <li key={index}>{todo}</li>
+))}
+</ol>
+</div>
     </>
   )
 }
